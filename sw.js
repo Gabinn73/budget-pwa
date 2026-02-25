5f0b68473abe89cb12abf461d294aa7ed46f99b5
Gabinn73 <132207597+Gabinn73@users.noreply.github.com>
2026-02-25 13:45:33 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = "budget-pwa-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+});
