578c5d51f9bc5fc9a6f79c5fecf15ffaf75fb452
Gabinn73 <132207597+Gabinn73@users.noreply.github.com>
2026-02-25 13:57:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
-const CACHE = "budget-pwa-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json"];
+const CACHE = "budget-pwa-v2";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    await self.clients.claim();
+  })());
 });
 
 self.addEventListener("fetch", (event) => {
